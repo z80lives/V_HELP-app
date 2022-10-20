bb6b8d21d583af68e8e235b9852cb15b1cb4bbac
Ali <ali.mirghafouri@gmail.com>
2022-10-20 14:13:16 +0800

    notify for school

--- src/app/manage-school/manage-school.component.ts
+++ src/app/manage-school/manage-school.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import {NewSchoolService} from '../core/services/new-school.service';
+import { NewSchoolService } from '../core/services/new-school.service';
 import { Router } from '@angular/router';
-import {NewSchool} from "../tools/tools/api/models/new-school";
+import { NewSchool } from '../tools/tools/api/models/new-school';
+import { CoreDataService } from '../core/services/core-data.service';
 
 @Component({
   selector: 'app-school-manage',
@@ -17,7 +18,8 @@ export class ManageSchoolComponent implements OnInit {
   constructor(
     private readonly _router: Router,
     private readonly _fb: FormBuilder,
-    private readonly _schoolService: NewSchoolService
+    private readonly _schoolService: NewSchoolService,
+    private readonly _core: CoreDataService
   ) {}
   addSchoolForm = this._fb.group({
     schoolName: '',
@@ -29,15 +31,13 @@ export class ManageSchoolComponent implements OnInit {
 
   onClickSubmit($event: SubmitEvent) {
     $event.preventDefault();
-    const formData : NewSchool = this.addSchoolForm.value as any;
+    const formData: NewSchool = this.addSchoolForm.value as any;
 
     this._schoolService.create(formData).subscribe((result) => {
       if (result) {
-        alert('Successfully created');
+        this._core.notifyInfo('School Created', 'School Created');
         this.addSchoolForm.reset();
-        this._router.navigate([
-          '/dashboard/root'
-        ]);
+        this._router.navigate(['/dashboard/root']);
       }
     });
   }
